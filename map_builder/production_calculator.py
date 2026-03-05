d25ddd53f53780d01de1794873d174a7192035b6
myron-alexander <206006742+myron-alexander@users.noreply.github.com>
2026-03-05 21:49:40 +0200

    Implementing production calculator.

--- map_builder/production_calculator.py
+++ map_builder/production_calculator.py
@@ -8,8 +8,224 @@ __all__ = [
 
 import json
 from typing import Any
+from abc import ABC, abstractmethod
 
 from application_data import GameData, load_game_data
+
+#---------------------------------------------------------------------------------------------------
+
+def make_map_node_key(id:str, site_id:str, factory_id:str|None) -> str:
+    return f"{site_id};{factory_id or ""};{id}"
+
+#---------------------------------------------------------------------------------------------------
+
+class MapNode(ABC):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, id:str, site_id:str, factory_id:str|None = None) -> None:
+        self.key:str = make_map_node_key(id, site_id, factory_id)
+        self.site_id = site_id
+        self.factory_id = factory_id or ""
+        self.node_id = id
+        self.inputs:list["MapNode"] = []
+        # Presume not part of any input until otherwise known.
+        self._terminal = True
+
+    #---------------------------------------------------------------------------
+
+    @property
+    @abstractmethod
+    def is_boundary(self) -> bool:
+        """
+        True when node is an entry point into the factory, or out of the factory. This is
+        intended to be used when traversing the tree, to detect the crossing point from one
+        factory to another without having to check the node type.
+        """
+        pass
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_terminal(self) -> bool:
+        """
+        A node that is at the end of a production chain is called a terminal.
+        Only crafters, storage and dispatchers can be at the end of a production chain so only
+        they can be terminal. All such nodes are considered terminal until they supply items
+        to a consumer, in which case they are not terminal.
+        """
+        return self._terminal
+
+    #---------------------------------------------------------------------------
+
+    def set_not_terminal(self) -> None:
+        self._terminal = False
+
+    #---------------------------------------------------------------------------
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    #---------------------------------------------------------------------------
+
+    def __repr__(self) -> str:
+        return \
+            f"{type(self).__name__} [\n"\
+            f"   key     : {self.key}\n"\
+            f"   terminal: {self._terminal}\n"\
+            f"   inputs  : {[n.key for n in self.inputs]}\n"\
+             "]\n"
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapResourceNode(MapNode):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, site_id:str, resource_id:str) -> None:
+        super().__init__(resource_id, site_id)
+        # A resource cannot be a terminal, only dispatcher, storage and crafter.
+        self._terminal = False
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_boundary(self) -> bool:
+        return False
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapCrafterNode(MapNode):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, site_id:str, factory_id:str, crafter_id:str) -> None:
+        super().__init__(crafter_id, site_id, factory_id)
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_boundary(self) -> bool:
+        return False
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapStorageNode(MapNode):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, site_id:str, factory_id:str, storage_id:str) -> None:
+        super().__init__(storage_id, site_id, factory_id)
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_boundary(self) -> bool:
+        return False
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapDispatcherNode(MapNode):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, site_id:str, factory_id:str, dispatcher_id:str) -> None:
+        super().__init__(dispatcher_id, site_id, factory_id)
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_boundary(self) -> bool:
+        return True
+
+    #---------------------------------------------------------------------------
+
+    def set_not_terminal(self) -> None:
+        # A dispatcher is always terminal within the factory.
+        return
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapReceiverNode(MapNode):
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self, site_id:str, factory_id:str, receiver_id:str) -> None:
+        super().__init__(receiver_id, site_id, factory_id)
+        # A receiver cannot be a terminal, only dispatcher, storage and crafter.
+        self._terminal = False
+
+    #---------------------------------------------------------------------------
+
+    @property
+    def is_boundary(self) -> bool:
+        return True
+
+    #---------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------------------------
+
+class MapNetwork:
+    """
+    """
+
+    #---------------------------------------------------------------------------
+
+    def __init__(self) -> None:
+        self.map_nodes:dict[str,MapNode] = {}
+
+    #---------------------------------------------------------------------------
+
+    def add_node(self, node:MapNode) -> None:
+        self.map_nodes[node.key] = node
+
+    #---------------------------------------------------------------------------
+
+    def get_factory_node(self, site_id:str, factory_id:str, id:str) -> MapNode:
+        key = make_map_node_key(id, site_id, factory_id)
+        return self.map_nodes[key]
+
+    #---------------------------------------------------------------------------
+
+    def get_site_node(self, site_id:str, id:str) -> MapNode:
+        key = make_map_node_key(id, site_id, None)
+        return self.map_nodes[key]
+
+    #---------------------------------------------------------------------------
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    #---------------------------------------------------------------------------
+
+    def __repr__(self) -> str:
+        nodes = [n for n in self.map_nodes.values()]
+        nodes.sort(key=lambda n:n.key)
+        return "\n".join([str(n) for n in nodes])
+
+    #---------------------------------------------------------------------------
 
 #---------------------------------------------------------------------------------------------------
 
@@ -41,11 +257,14 @@ class NodeLedgerEntry:
         actual_production_rate_ipm is less than baseline_production_rate_ipm, then these
         values will be reduced to scale.
         """
+        self.required_ipm:dict[str, int] = {}
+        """
+        """
         self.capacity_ipm:int = 0
         """
         The total amount able to be supplied. If consumers attempt to pull more then the
-        baseline_production_rate_ipm, capacity_ipm will be negative for the amount over
-        baseline_production_rate_ipm.
+        actual_production_rate_ipm, capacity_ipm will be negative for the amount over
+        actual_production_rate_ipm.
         """
 
     #---------------------------------------------------------------------------
@@ -121,6 +340,65 @@ class NodeLedger:
 
     def __init__(self) -> None:
         self.ledger:dict[str, NodeLedgerEntry] = {}
+        self.factory_production_target_ipm:dict[str, dict[str,int]] = {}
+        """
+        In order to calculate usage rates, it must be possible to set a target production rate
+        for an item of a factory. The key is "site_id;factory_id" and the value is a mapping from
+        item to target rate ipm.
+        """
+
+    #---------------------------------------------------------------------------
+
+    def set_factory_target_rate(
+            self, site_id:str, factory_id:str, item_id:str, target_rate_ipm:int) -> None:
+        """
+        Set the target rate for an item produced in a factory.
+
+        Parameters
+        ---------
+        site_id : str
+            Identifies the site.
+
+        factory_id : str
+            Identifies the factory.
+
+        item_id : str
+            Identifies the item which must be produced at the target rate.
+
+        target_rate_ipm : int
+            The rate, in items per minute, at which the factory must produce the item.
+        """
+        key = f"{site_id};{factory_id}"
+        target_ipms = self.factory_production_target_ipm.setdefault(key, {})
+        target_ipms[item_id] = target_rate_ipm
+
+    #---------------------------------------------------------------------------
+
+    def remove_factory_target_rate(
+            self, site_id:str, factory_id:str, item_id:str|None = None) -> None:
+        """
+        Remove the target rate for an item, or all items, of a factory.
+
+        Parameters
+        ---------
+        site_id : str
+            Identifies the site.
+
+        factory_id : str
+            Identifies the factory.
+
+        item_id : str|None
+            When provided, identifies the item whose target rate is to be removed. If not provided,
+            the entire factory is removed from the factory target rate collection.
+        """
+        key = f"{site_id};{factory_id}"
+        if key in self.factory_production_target_ipm:
+            if item_id is not None:
+                target_ipms = self.factory_production_target_ipm[key]
+                if item_id in target_ipms:
+                    del target_ipms[item_id]
+            else:
+                del self.factory_production_target_ipm[key]
 
     #---------------------------------------------------------------------------
 
@@ -244,10 +522,24 @@ def populate_defined_production_rates(
 
 #---------------------------------------------------------------------------------------------------
 
-def calculate_factory_usage_rates(site_id:str, factory_id:str):
+def calculate_factory_production_and_pull_rates(
+        site_id:str, factory_id:str,map_data:dict[str,Any], ledger:NodeLedger) -> None:
+    """
+    Calculate the actual production and pull rates for nodes in the factory based on the
+    set target rates, or pull request from another factory depending on which is greater.
+
+    If the factory has neither a set target rate, nor a dispatcher for a terminal item producer,
+    then the actual production rate and pull rates for that terminal item producer will be zero.
+    A terminal item producer is one that produces an item and is not an input into either another
+    producer, or storage.
     """
 
-    """
+    # 1. Find all the terminal producers.
+    # 2. For each terminal producer, set the required_ipm from either the factory target or
+    #    the dispatcher.
+    # 3. Iterate through the production tree setting the required ipm.
+    # 4. Once all the required ipm values have been set, calculate the
+
 
 #---------------------------------------------------------------------------------------------------
 
@@ -272,16 +564,95 @@ def calculate_factory_input_rates(site_id:str, factory_id:str):
 
 #---------------------------------------------------------------------------------------------------
 
+def build_map_network(map_data:dict[str,Any]) -> MapNetwork:
+
+    map_network = MapNetwork()
+
+    #
+    # Create the nodes in the network.
+    #
+
+    for site_id, site_values in map_data.items():
+        for resource_id in site_values.get("resource_nodes", {}).keys():
+            map_network.add_node(MapResourceNode(site_id, resource_id))
+        for factory_id, factory_values in site_values.get("factories", {}).items():
+            machines = factory_values.get("machines", {})
+            for crafter_id in machines.get("crafters", {}).keys():
+                map_network.add_node(MapCrafterNode(site_id, factory_id, crafter_id))
+            for storage_id in machines.get("storage", {}).keys():
+                map_network.add_node(MapStorageNode(site_id, factory_id, storage_id))
+            for displatcher_id in factory_values.get("dispatchers", {}).keys():
+                map_network.add_node(MapDispatcherNode(site_id, factory_id, displatcher_id))
+            for receiver_id in factory_values.get("receivers", {}).keys():
+                map_network.add_node(MapReceiverNode(site_id, factory_id, receiver_id))
+
+    print(map_network)
+
+    #
+    # Link the nodes.
+    #
+
+    def get_input_node(site_id, factory_id, input_id, resource_ids) -> MapNode:
+        if input_id in resource_ids:
+            node = map_network.get_site_node(site_id, input_id)
+        else:
+            node = map_network.get_factory_node(site_id, factory_id, input_id)
+        # Node is now known as an input.
+        node.set_not_terminal()
+        return node
+
+    for site_id, site_values in map_data.items():
+        resource_ids = [k for k in site_values.get("resource_nodes", {})]
+        
+        for factory_id, factory_values in site_values.get("factories", {}).items():
+            machines = factory_values.get("machines", {})
+
+            for crafter_id, crafter_values in machines.get("crafters", {}).items():
+                crafter_node = map_network.get_factory_node(site_id, factory_id, crafter_id)
+                for ii in crafter_values["inputs"]:
+                    for input_id in ii["from_ids"]:
+                        crafter_node.inputs.append(
+                            get_input_node(site_id, factory_id, input_id, resource_ids))
+
+            for storage_id, storage_values in machines.get("storage", {}).items():
+                storage_node = map_network.get_factory_node(site_id, factory_id, storage_id)
+                for ii in storage_values["inputs"]:
+                    for input_id in ii["from_ids"]:
+                        storage_node.inputs.append(
+                            get_input_node(site_id, factory_id, input_id, resource_ids))
+
+            for displatcher_id, dispatcher_values in factory_values.get("dispatchers", {}).items():
+                dispatcher_node = map_network.get_factory_node(site_id, factory_id, displatcher_id)
+                for input_id in dispatcher_values["from_ids"]:
+                    dispatcher_node.inputs.append(
+                        get_input_node(site_id, factory_id, input_id, resource_ids))
+            
+            for receiver_id, receiver_values in factory_values.get("receivers", {}).items():
+                receiver_node = map_network.get_factory_node(site_id, factory_id, receiver_id)
+                other_site_id = receiver_values["site_id"]
+                other_factory_id = receiver_values["factory_id"]
+                other_dispatcher_id = receiver_values["dispatcher_id"]
+                receiver_node.inputs.append(
+                    get_input_node(
+                        other_site_id, other_factory_id, other_dispatcher_id, resource_ids))
+            
+    return map_network
+
+
+#---------------------------------------------------------------------------------------------------
+
 def main():
     game_data = load_game_data()
 
     with open('pins_data.json', 'r', encoding='utf-8') as f:
         map_data = json.load(f)
-    
-    ledger = NodeLedger()
-    populate_defined_production_rates(map_data, game_data, ledger)
 
-    print(ledger.ledger)
+    # ledger = NodeLedger()
+    # populate_defined_production_rates(map_data, game_data, ledger)
+    # print(ledger.ledger)
+
+    map_network = build_map_network(map_data)
+    print(map_network)
 
 
 #---------------------------------------------------------------------------------------------------
